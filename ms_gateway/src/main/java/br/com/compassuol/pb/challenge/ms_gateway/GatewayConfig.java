0ae98c2c4f29c933b29cd02e71e18b9b04d4ed9c
TaylorHudson <taylorhudsonsantos@gmail.com>
2023-07-04 16:14:59 -0300

    Refactor: changes the gateway to work with user authentication

--- ms_gateway/src/main/java/br/com/compassuol/pb/challenge/ms_gateway/GatewayConfig.java
+++ ms_gateway/src/main/java/br/com/compassuol/pb/challenge/ms_gateway/GatewayConfig.java
@@ -1,21 +1,17 @@
 package br.com.compassuol.pb.challenge.ms_gateway;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class GatewayConfig {
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("ms-products", r -> r
-                        .path("/products/**")
-                        .filters(f -> f.rewritePath("/products/(?<id>.*)", "/products/${id}"))
-                        .uri("http://localhost:8080"))
-                .build();
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
     }
 
 }
