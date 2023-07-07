2741128cfa0c72807f4617e13f5e266092df9d3c
TaylorHudson <taylorhudsonsantos@gmail.com>
2023-07-07 11:27:34 -0300

    Refactor: correcting permissions for access to category endpoints

--- ms-products/src/main/java/br/com/compassuol/pb/challenge/msproducts/framework/adapters/in/CategoryController.java
+++ ms-products/src/main/java/br/com/compassuol/pb/challenge/msproducts/framework/adapters/in/CategoryController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,23 +19,27 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CategoryRequest request) {
         var response = categoryService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<CategoryResponse>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(categoryService.findAll());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<CategoryResponse> update(@PathVariable Long id, @Valid @RequestBody CategoryRequest request) {
         var response = categoryService.update(id, request);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
         var response = categoryService.deleteById(id);
